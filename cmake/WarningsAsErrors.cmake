f96d0c99af54cbbcae4aaf0a2b8eac4d65af8624
ClausKlein <claus.klein@arcormail.de>
2022-03-14 21:45:05 +0100

    modernize cmake
    NOTE: fails on OSX with clang++ v13

---
+++ cmake/WarningsAsErrors.cmake
@@ -0,0 +1,30 @@
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_SIMULATE_ID MATCHES "MSVC")
+    target_compile_options(${PROJECT_NAME} INTERFACE
+            /W4
+            /WX
+            )
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    target_compile_options(${PROJECT_NAME} INTERFACE
+            -Wall
+            -Wextra
+            -Wpedantic
+            #TODO -Werror
+            -Wshadow
+            )
+
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 10.0)
+        target_compile_options(${PROJECT_NAME} INTERFACE
+                -Wdeprecated-copy-dtor
+                -Wdeprecated-copy
+                -Warray-bounds
+                -Wunused-value
+                -Wnewline-eof
+                )
+        endif ()
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    string(REGEX REPLACE " /W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    target_compile_options(${PROJECT_NAME} INTERFACE
+            /W4
+            /WX
+            )
+endif ()
