2eb94eee9f03bc0a8ec96aca5e5a1868d8f5a638
ClausKlein <claus.klein@arcormail.de>
2022-04-09 13:38:35 +0200

    add cmake-format config file
    reformat all cmake files

--- cmake/WarningsAsErrors.cmake
+++ cmake/WarningsAsErrors.cmake
@@ -2,15 +2,14 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_SIMULATE_ID MATCHES "MSVC
   target_compile_options(${PROJECT_NAME} INTERFACE /W4 /WX)
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
   target_compile_options(
-    ${PROJECT_NAME} INTERFACE -Wall -Wextra -Wpedantic
-                      # FIXME -Werror
-                      # TODO: -Wshadow
+    ${PROJECT_NAME} INTERFACE -Wall -Wextra -Wpedantic # FIXME -Werror
+                                                       # TODO: -Wshadow
   )
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 10.0)
     target_compile_options(
-      ${PROJECT_NAME} INTERFACE -Wdeprecated-copy-dtor -Wdeprecated-copy
-                                -Warray-bounds -Wunused-value -Wnewline-eof)
+      ${PROJECT_NAME} INTERFACE -Wdeprecated-copy-dtor -Wdeprecated-copy -Warray-bounds -Wunused-value -Wnewline-eof
+    )
   endif()
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   # TODO: string(REGEX REPLACE " /W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
